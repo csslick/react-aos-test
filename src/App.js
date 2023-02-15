e297a6babd9153570979055914ceabed4e6354f7
JuHwan <juhwan203@kakao.com>
2023-02-15 13:01:47 +0900

    react aos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import rocket from './rocket.png'
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='s1'>
+        <h1>section 1</h1>
+      </section>
+      <section className='s2'>
+        <h1 data-aos='fade-left'>section 2</h1>
+      </section>
+      <section className='s3'>
+        <div>
+          <h1 
+            data-aos="fade-down" 
+            data-aos-delay="1500">위대한 도약</h1>
+          {/* custom ani */}
+          <img 
+            data-aos 
+            data-aos-duration='3000'
+            data-aos-delay='0'
+            className='rocket'
+            style={{
+              width: '600px'
+            }}
+            src={rocket} alt="rocket" />
+        </div>
+      </section>
     </div>
   );
 }
